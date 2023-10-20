96c627b90c5231ec6df19b6b6abb7146fb1f3c5d
fujimura00 <marusannmk2@gmail.com>
2023-10-20 14:54:25 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 "use strict"
 
 const header = document.querySelector('header');
+const body = document.querySelector('body');
+
 let judge = false;
 window.addEventListener('scroll', () => {
     if(window.pageYOffset < 150){
         header.classList.remove("menuAnime");
+        body.classList.remove("bgLight")
         if(judge == true){
             header.classList.add("menuBackAnime");
+            body.classList.add("bgOrigin")
         }
+    
+
+
     }else{
         header.classList.add("menuAnime");
         header.classList.remove("menuBackAnime");
+        body.classList.add("bgLight")
+        body.classList.remove("bgOrigin")
         judge = true;
-    }
-})
 
+    }
+
+
+})
