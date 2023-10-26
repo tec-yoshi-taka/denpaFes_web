922827efb679e87e8976a28786bc7ed0db639aac
haruta-kun <ohtaharuga@gmail.com>
2023-10-26 14:01:06 +0900

    1401

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@
 
 const header = document.querySelector('header');
 const body = document.querySelector('body');
+const hMenu = document.querySelector('.h-menu');
+const hMenuBtn = document.querySelector('.h-menu-btn');
+
+hMenu.addEventListener('mousedown', () => {
+    header.classList.toggle('up');
+    hMenuBtn.classList.toggle('up');
+});
 
 let judge = false;
 window.addEventListener('scroll', () => {
@@ -12,15 +19,15 @@ window.addEventListener('scroll', () => {
             header.classList.add("menuBackAnime");
             body.classList.add("bgOrigin")
         }
-    
+        
     }else{
         header.classList.add("menuAnime");
         header.classList.remove("menuBackAnime");
         body.classList.add("bgLight")
         body.classList.remove("bgOrigin")
         judge = true;
-
+        
     }
-
-
-})
+});
+    
+    
